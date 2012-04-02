07add491585a457e3934754af206eb5f8bf90aee
George Ciobanu <geo.ciobanu@gmail.com>
2012-04-02 00:17:02 -0700

    Initial Rails app

---
+++ callme/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callme::Application.config.secret_token = '34422bebcec344c39a6cc32f935dc5a1f73424fa3f76bf76907e291804e3e5dcfc12478506a27e1ca088beceabb46c56d1317bf7e665bbc56646019a18080781'
