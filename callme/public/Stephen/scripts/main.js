04291c0da2e042516133f8f21a29fc9cd26ec060
Stephen Etheredge <stephen@passfail.com>
2013-01-25 03:58:52 -0800

    upload window, note detail view, team module

--- callme/public/Stephen/scripts/main.js
+++ callme/public/Stephen/scripts/main.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
+	
+	document.body.style.overflow = 'hidden';
+	
 	// Load tooltips
 	$('.tooltip2').tooltip('hide')
 	// Load popovers
-	$('.popover2').popover({ 
-		trigger: "hover",
-		delay: 300, 
-	})
+
 	
-	$('.popover3').popover({ 
-		trigger: "click",
-		delay: 100, 
-	})
 	
+	$('.popover2').popover({
+	    html: true,
+	    trigger: 'hover',
+	});
+
+
+
+
+	
+	
+	
+
 	$('.modal2').click(function() {
 	  $('.popover2').popover('hide');
+	  $('#invitePop').popover('hide');
+	  $('#sharePopover').popover('hide');
 	});
 	
 	
@@ -27,16 +37,64 @@ $(document).ready(function() {
 	});
 	
 	
-	$('.userThumb').click(function() {
-	  $('.userDetail').toggleClass('hide');
-	});
+	// Add class to share popover
 	
 	
+	    $('#sharePopover')
+		    		    
+		    .popover({
+		        placement : 'bottom',
+		        html : true,
+		        title : 'Paste this code on your site or blog.',
+		        placement : 'bottom',
+		        trigger : 'click',
+		        content : "<h6>Paste this code on your site or blog</h6><form><textarea class='code'><iframe width='560' height='315' src='https://sunglass.io/player/e5b78dc8-150c-4a19-9f02-36763f80ac5b' frameborder='0' allowfullscreen mozallowfullscreen webkitallowfullscreen></iframe></textarea><div class='clearfix'><div class='btn-group pull-left'><a class='btn btn-small dropdown-toggle' data-toggle='dropdown' href='#'>Size <span class='caret'></span></a><ul class='dropdown-menu'><li><a href='#'>560x315</a></li><li><a href='#'>640x360</a></li><li><a href='#'>850x480</a></li><li><a href='#'>1280x720</a></li><li><a href='#'>Custom</a></li></ul></div><a href='#' class='btn pull-right btn-small'>Copy</a></div></form>"
+		  })
+		  .click(function() {
+		
+		$('.popover').addClass('sharePopWin');
+		$('#invitePop').popover('hide')
+		
+	})
 	
+		
+
+		  
+		  $('#invitePop').click(function() { 
+			  $('#sharePopover').popover('hide')
+		  })
+		  .popover({
+			  placement : 'bottom',
+		        html : true,
+		        title : 'Paste this code on your site or blog.',
+		        placement : 'bottom',
+		        trigger : 'click',
+		        content : "<form><input class='input-block-level' placeholder='email' type='email'><p>Search for a person in Sunglass by name or email address, or enter an email address to invite someone new.</p><a href='#' class='btn btn-success btn-block'>Send Invitations</a></form>"
+		  })
+		  
+});
+
+		  
+		  //Fix for team drop down menu and close function conflict
+
+	
+		$('#team .dropdown-toggle').click(function() {
+			$('#team.collapse.in').addClass('overflowVisible')
+			
+		})
+		
+		$('.hide-module').click(function() {
+			$('#team.collapse.in').removeClass('overflowVisible')
+			
+		})
+	
+
 	
 	//Hide Module Button
 	$('.hide-module').click('hide')
+
 	var bodyheight = $(document).height();
+	
 	$(".sidePanel").height(bodyheight);
 	
 	
@@ -55,7 +113,7 @@ $(document).ready(function() {
 		$("#addSpaceInput").addClass("hide");
 		$("#addSpace").removeClass("hide");
 	});
-})
+
 
 
 
